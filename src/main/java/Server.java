a58b7619c5a10d571ddaf721b05397e8b13dc3cb
Tobias Akesson <tobias.akesson0005@stud.hkr.se>
2018-11-08 10:59:27 +0100

    Added Gson library and code for splitting up Json strings
    Co-Authored-By: Peter Westin <pwhkr@users.noreply.github.com>
    Co-Authored-By: painedcarbon <painedcarbon@users.noreply.github.com>

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,4 +1,5 @@
 import DB.DBHandler;
+import com.google.gson.Gson;
 
 public class Server {
 
@@ -8,11 +9,41 @@ public class Server {
 
     public void run() {
         // 1: Ta emot json string
+
+        /*switch (type) {
+            case "changeLamp" :
+
+                break;
+            default :
+                System.out.println("Invalid type!");
+                break;
+            }*/
+
+        }
+
         // 2: Utför jobb på arduino
         // 3: Invänta reply från arduino
         // 4: Updatera databasen
         // 5: Uppdatera websidan
 
+        private String splitJsonString(String jsonString, String wantedValue) {
+            Gson gson = new Gson();
+            String textJson = gson.toJson(jsonString);
+            System.out.println(textJson);
+            String[] splittedJson = textJson.split("\\W");
+            String returnValue = "";
 
+            if (wantedValue == "type") {
+                returnValue = splittedJson[5];
+            }
+            if (wantedValue == "deviceId") {
+                returnValue = splittedJson[11];
+            }
+            if (wantedValue == "value") {
+                returnValue = splittedJson[17];
+            }
+
+            return returnValue;
+        }
     }
-}
+
